8160fa09e523d428695fadbee2c5af0c30e4b3e1
siddhantyadavgzp <42622546+siddhantyadavgzp@users.noreply.github.com>
2018-12-05 14:06:30 +0530

    #2 correction in drawer

--- app/src/main/java/com/apress/gerber/homework/MainActivity.java
+++ app/src/main/java/com/apress/gerber/homework/MainActivity.java
@@ -61,6 +61,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public void onBackPressed() {
         if (drawer.isDrawerOpen(GravityCompat.START))
             drawer.closeDrawer(GravityCompat.START);
+        else
         super.onBackPressed();
     }
 
