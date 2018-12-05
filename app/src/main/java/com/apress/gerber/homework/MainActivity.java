f96edca373fa9c5d6bc732602fd6e03d7f2e828b
siddhantyadavgzp <42622546+siddhantyadavgzp@users.noreply.github.com>
2018-12-05 13:46:06 +0530

    #1 commit to the fragments adding three fragments as listed

--- app/src/main/java/com/apress/gerber/homework/MainActivity.java
+++ app/src/main/java/com/apress/gerber/homework/MainActivity.java
@@ -1,5 +1,7 @@
 package com.apress.gerber.homework;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -7,25 +9,57 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar1= findViewById(R.id.toolbar);
+        Toolbar toolbar1 = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar1);
-        drawer=findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle= new ActionBarDrawerToggle(this, drawer,toolbar1,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar1, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new LoginFragment()).commit();
+            navigationView.setCheckedItem(R.id.login);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.login:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new LoginFragment()).commit();
+                break;
+            case R.id.recycle:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new RecyclerFragment()).commit();
+                break;
+            case R.id.blank:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new BlankFragment()).commit();
+                break;
+            case R.id.Exit:
+                Toast.makeText(MainActivity.this, "Sorry didn't had enough time to google how to close it", Toast.LENGTH_LONG).show();
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+
+
     }
 
     @Override
     public void onBackPressed() {
-        if(drawer.isDrawerOpen(GravityCompat.START))
+        if (drawer.isDrawerOpen(GravityCompat.START))
             drawer.closeDrawer(GravityCompat.START);
         super.onBackPressed();
     }
